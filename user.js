e4dd79c2d86cbded3081b97cfae1dcd7c6731091
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-07-28 11:50:20 +0600

    user.js: force dark mode on websites

--- user.js
+++ user.js
@@ -4,6 +4,7 @@ user_pref("svg.context-properties.content.enabled", true);
 
 // UI
 user_pref("browser.compactmode.show", true);
+user_pref("layout.css.prefers-color-scheme.content-override", 0); // Force dark mode on websites
 
 // Remove native context menus
 user_pref("widget.gtk.native-context-menus", false);
